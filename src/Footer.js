c55c0b21892a0fe8a50d5787457b5e72b14ea22d
Surya - Georgian <surya.tammana@georgiancollege.ca>
2021-03-14 16:47:35 -0400

    Added Footer to the Project.

--- src/Footer.js
+++ src/Footer.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React from 'react';
+import linkedin from './images/linkedin.svg';
+import github from './images/github-sign.svg';
+import './Footer.scss';
 
 function Footer() {
     return (
         <div className="footer">
             {/* Github and Linkedin Page Icons */}
+            <div className="footer__social">
+                <a href="https://www.linkedin.com/in/suryatejatammana/" target="_blank" rel="noreferrer">
+                    <img src={linkedin} alt="linkedin-logo" />
+                </a>
+                <a href="https://github.com/imSurya-dev" target="_blank" rel="noreferrer" >
+                    <img src={github} alt="github-logo" />
+                </a>
 
+            </div>
             {/* Portfolio Page */}
-            {/* mention TMDB API */}
-
+            <div className="footer__portfolio">
+                <a href="http://surya-teja.surge.sh" target="_blank" rel="noreferrer" >Checkout My Portfolio</a>
+            </div>
+            <div>
+                Developed by Surya Teja Tammana
+            </div>
         </div>
     )
 }
